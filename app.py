ad329717a822b3292687519bb4f6859ffbf84438
torshin5ergey <ttorshin5ergey@gmail.com>
2025-04-03 19:24:42 +0300

    refactor: add ping, shutdown, remove root, add tests

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+
+import os
+import signal
+import threading
+import logging
+import time
+
 from flask import Flask
+
+log = logging.basicConfig(
+    level=logging.INFO,
+    format="%(name)s %(levelname)s %(asctime)s    "
+           "%(message)s",
+    datefmt="%I:%M:%S"
+)
+log = logging.getLogger()
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello world\n"
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong", 200
+
+@app.route("/shutdown", methods=["POST"])
+def shutdown():
+    def kill_with_delay():
+        time.sleep(1)
+        os.kill(os.getpid(), signal.SIGKILL)
+
+    threading.Thread(target=kill_with_delay).start()
+
+    return "Shutting down...", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7777)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,13 @@
+
+import os
+import signal
+import time
+
+from unittest.mock import patch
 import pytest
+
 from app import app
+
 
 @pytest.fixture
 def client():
@@ -7,15 +15,27 @@ def client():
     with app.test_client() as client: # create test client
         yield client
 
-def test_root(client):
+def test_ping(client):
     # GET to url "/"
-    response = client.get("/")
+    response = client.get("/ping")
 
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "hello world\n"
+    assert response.data.decode("utf-8") == "pong"
     assert "text/html" in response.content_type
 
 def test_nonexists(client):
     response = client.get("/nonexists")
 
     assert response.status_code == 404
+
+def test_shutdown(client):
+    # замена реального os.kill на мок объект
+    with patch("os.kill") as mock_kill:
+        response = client.post("/shutdown")
+
+        assert response.status_code == 200
+        assert response.data.decode("utf-8") == "Shutting down..."
+        assert "text/html" in response.content_type
+        time.sleep(1.5)
+        # assert_called_once_with функция вызвана один раз с аргементами
+        mock_kill.assert_called_once_with(os.getpid(), signal.SIGKILL)
